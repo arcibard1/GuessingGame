d5b1e5218261c98e1a73a4b5418f9b1bb52aeee2
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-05-04 09:59:50 -0700

    framework for game

---
+++ game.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("#guess");
+const button = document.querySelector("#guessButton");
+const tracker = document.querySelector("#guessTracker");
+
+
+let answer = Math.floor(Math.random() * 100);
