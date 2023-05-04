439e8d3fc189798d6f12dc3e7d741c4b43371f56
Timothy Chang <123012347+Proton2050@users.noreply.github.com>
2023-05-04 13:29:13 -0700

    all working

--- game.js
+++ game.js
@@ -2,7 +2,8 @@ const input = document.querySelector("#guess");
 const button = document.querySelector("#guessButton");
 const tracker = document.querySelector("#guessTracker");
 const message = document.querySelector("#message");
-
+const reset = document.querySelector("#reset");
+const body = document.body
 
 let answer = Math.floor(Math.random() * 101);
 let attempts = 5;
@@ -29,10 +30,12 @@ button.addEventListener("click", function(){
     if (guess === answer){
         message.textContent = "Congrats! You got it right!";
         button.disabled = true;
+        body.style.backgroundColor = 'gold';
         // button.style.color
     } else if (attempts === 0){
         message.textContent = `Game Over. The correct answer was ${answer}.`;
         button.disabled = true;
+        body.style.backgroundColor = 'red';
     } else{
         let hint; 
         if (guess < answer){
@@ -40,8 +43,31 @@ button.addEventListener("click", function(){
         } else {
             hint = "Lower"
         } 
-        message.textContent = `Try again! ${hint}`;
+        let distance = answer - guess;
+        if (distance < 0) {
+            distance = distance * -1;
+        }
+        let temp = '';
+        if (distance <= 7) {
+            temp = 'very hot';
+        } else if (distance <= 14) {
+            temp = 'getting warm';
+        } else {
+            temp = 'cold';
+        }
+        message.textContent = `Try again! ${hint}. You're ${temp}.` ;
     }
 })
+
+reset.addEventListener("click", function () {
+    answer = Math.floor(Math.random() * 101);
+    attempts = 5;
+    pastGuesses = [];
+    tracker.textContent = "Previous Guesses: ";
+    message.textContent = "";
+    body.style.backgroundColor = "";
+    button.disabled = false;
+    input.value = "";
+});
 
 
