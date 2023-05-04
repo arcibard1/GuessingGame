3a38db75321f683512f70598e5048f5c127a3283
David Arcibar <davidaarcibar@gmail.com>
2023-05-04 14:05:26 -0500

    updatedcode

--- game.js
+++ game.js
@@ -1,6 +1,29 @@
 const input = document.querySelector("#guess");
 const button = document.querySelector("#guessButton");
 const tracker = document.querySelector("#guessTracker");
+const message = document.querySelector("#message");
 
 
-let answer = Math.floor(Math.random() * 100);
+let answer = Math.floor(Math.random() * 101);
+let attempts = 5;
+let pastGuesses = [];
+input.appendChild(message);
+/*
+1. intialize a variable to keep track of the number of attempts
+and an array to store the previous guesses
+2. add an event listner for the button click event
+3. 
+*/
+
+button.addEventListener("click", function(){
+    let guess = parseInt(input.value);
+    pastGuesses.push(guess);
+    tracker.innerText = "Previous Guesses: " + pastGuesses;
+    // console.log(pastGuesses);
+})
+console.log("finish")
+
+if (guess < 1 || guess < 100) {
+    message.textContent = "Guess your number within the Range!";
+    console.log(message);
+}
